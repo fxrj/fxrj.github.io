d5d9698ce91650a93add8c8d86e3719cf4f05ea3
ihonux <103343839+ihonux@users.noreply.github.com>
2023-12-25 10:55:22 +0800

    update Twikoo

--- static/js/post.js
+++ static/js/post.js
@@ -49,7 +49,7 @@ function renderComment() {
         // path: location.pathname, // 用于区分不同文章的自定义 js 路径，如果您的文章路径不是 location.pathname，需传此参数
         // lang: 'zh-CN', // 用于手动设定评论区语言，支持的语言列表 https://github.com/imaegoo/twikoo/blob/main/src/client/utils/i18n/index.js
     });
-    $("#disqus_thread").removeClass('hidden');
+    $("#post-comment").removeClass('hidden');
 }
 
 
