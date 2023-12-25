be049fb9b9602e8b7f36f74979e0d9e73cab665d
ihonux <103343839+ihonux@users.noreply.github.com>
2023-12-25 10:42:55 +0800

    update

--- static/js/post.js
+++ static/js/post.js
@@ -6,8 +6,8 @@
  * 页面ready方法
  */
 $(document).ready(function() {
-    generateContent();
-    // share();
+    //generateContent();
+    //// share();
     renderComment();
 });
 
@@ -29,7 +29,7 @@ function share(){
 
 
 function renderComment() {
-    var gittalk = new Gitalk({
+    /*var gittalk = new Gitalk({
         id: md5(window.location.pathname),
         clientID: '{{site.comment.client_id}}',
         clientSecret: '{{site.comment.client_secret}}',
@@ -40,6 +40,15 @@ function renderComment() {
         distractionFreeMode: false
     });
     gittalk.render('post-comment')
+    $("#post-comment").removeClass('hidden');*/
+
+    twikoo.init({
+        envId: 'https://comment.ihonux.com', // 腾讯云环境填 envId；Vercel 环境填地址（https://xxx.vercel.app）
+        el: '#post-comment', // 容器元素
+        // region: 'ap-guangzhou', // 环境地域，默认为 ap-shanghai，腾讯云环境填 ap-shanghai 或 ap-guangzhou；Vercel 环境不填
+        // path: location.pathname, // 用于区分不同文章的自定义 js 路径，如果您的文章路径不是 location.pathname，需传此参数
+        // lang: 'zh-CN', // 用于手动设定评论区语言，支持的语言列表 https://github.com/imaegoo/twikoo/blob/main/src/client/utils/i18n/index.js
+    });
     $("#post-comment").removeClass('hidden');
 }
 
